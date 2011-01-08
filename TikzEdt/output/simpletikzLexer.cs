bf3781ace4557a140c239181da77dc3ce33695c1
notdifficult <notdifficult@gmx.net>
2011-01-08 20:16:15 +0000

    correct some grammar error add PreviewEnvironment code in stand alone mode

--- TikzEdt/output/simpletikzLexer.cs
+++ TikzEdt/output/simpletikzLexer.cs
@@ -1,4 +1,4 @@
-// $ANTLR 3.2 Sep 23, 2009 12:02:23 C:\\Eigene Dateien\\Eigene Programme\\C#\\tikzedt\\TikzEdt\\simpletikz.g 2011-01-08 19:37:43
+// $ANTLR 3.2 Sep 23, 2009 12:02:23 C:\\Eigene Dateien\\Eigene Programme\\C#\\tikzedt\\TikzEdt\\simpletikz.g 2011-01-08 20:16:32
 
 // The variable 'variable' is assigned but its value is never used.
 #pragma warning disable 168, 219
