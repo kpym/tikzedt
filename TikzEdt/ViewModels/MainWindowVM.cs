43081aecafb3156010ca9b136555016f01c0929d
t.willwacher <t.willwacher@e86494d1-7b75-2f4f-90ee-434e41cb6ced>
2011-02-13 15:37:15 +0000

    first steps towards mdi

---
+++ TikzEdt/ViewModels/MainWindowVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace TikzEdt.ViewModels
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        public ObservableCollection<TEDocumentVM> Documents { get;  private set; }
+
+        public TEDocumentVM ActiveDocument { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
