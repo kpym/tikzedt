9427d4c7f764e60c3c92975099998301d525a2be
t.willwacher <t.willwacher@e86494d1-7b75-2f4f-90ee-434e41cb6ced>
2012-04-29 22:38:22 +0000

    experiments

---
+++ TikzEdtGTK/GlobalUIGTK.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TikzEdt;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using Gtk;
+
+namespace TikzEdtGTK
+{
+    public class GlobalUIGTK : GlobalUIBase, IGlobalUI
+    {
+        /// <summary>
+        ///  It is mandatory that this is set to the main window before using GlobalUI.
+        /// </summary>
+        public Window MainForm = null;
+
+        //FileChooserDialog sfd = new FileChooserDialog() { OverwritePrompt = true, ValidateNames = true, AddExtension = true };
+        //FileChooserDialog ofd = new FileChooserDialog() { CheckFileExists = true, CheckPathExists = true };
+
+        public DialogResult ShowMessageBox(string Text, string Caption, MessageBoxButtons Button, MessageBoxIcon Icon)
+        {
+            var m = new MessageDialog(MainForm, DialogFlags.Modal, MessageType.Warning, ButtonsType.Ok, "Hallo Welt");
+            m.Run();
+            m.Destroy();
+            return DialogResult.OK;
+        }
+
+        public bool? ShowOpenFileDialog(out string FileName, string Filter = Consts.StdFileDialogFilter)
+        {
+            var f = new FileChooserDialog("Open File", MainForm, FileChooserAction.Open);
+            //f.CurrentFolder = Directory.GetCurrentDirectory();
+            //f.Filters = 
+            //f.Filter = Filter;
+            //bool? ret = ofd.ShowDialog(MainForm) == DialogResult.OK;
+            //FileName = ofd.FileName;
+
+            f.Run();
+            FileName = f.Filename;
+            f.Destroy();
+            return true;
+        }
+
+        public bool? ShowSaveFileDialog(out string FileName, string InitFilename, string Filter = Consts.StdFileDialogFilter)
+        {
+            /*sfd.InitialDirectory = Directory.GetCurrentDirectory();
+            sfd.FileName = InitFilename;
+            sfd.Filter = Filter;
+            bool? ret = sfd.ShowDialog(MainForm) == DialogResult.OK;
+            FileName = sfd.FileName;
+            return ret;*/
+
+            var f = new FileChooserDialog("Save File", MainForm, FileChooserAction.Open);
+            f.Run();
+            FileName = f.Filename;
+            f.Destroy();
+            return true;
+        }
+
+        public DialogResult ShowInputDialog(string Title, string Message, out string Text)
+        {
+            Text = "";
+            return DialogResult.OK;
+            //return InputDialog.ShowInputDialog(MainForm, Title, Message, out Text);
+            //return (Text == null ? DialogResult.Cancel : DialogResult.OK);
+        }
+
+        public void InvokeInUIThread(System.Action a)
+        {
+            Gtk.Application.Invoke(delegate { a(); });
+        }
+
+        public void BeginInvokeInUIThread(System.Action a)
+        {
+            // hack
+            Gtk.Application.Invoke(delegate { a(); });
+        }
+
+        public GlobalUIGTK()
+        {
+            //this.MainForm = MainForm;
+        }
+
+    }
+}
---
+++ TikzEdtGTK/ScintillaDocumentWrapper.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace TikzEdtGTK
+{
+    public class ScintillaDocumentWrapper : TikzEdt.ViewModels.ITEDoc, IDisposable
+    {
+        //public IDocument Document {get; private set; }
+
+        /// <summary>
+        /// Must be filled with a reference to the Texteditor control.
+        /// Its Document is recycled, instead of creating new documents.
+        /// (This is a hack and undesirable, but it seems one cannot set the ICSharpCode.Texteditor.Document to a newly created document.)
+        /// TODO: the document must be disposed of to avoid memory leak!!
+        /// </summary>
+        public static Sansguerre.GtkScintilla TheOneAndOnly;
+        public static ScintillaDocumentWrapper PreviousInstance = null;
+
+        public ScintillaDocumentWrapper()
+        {
+            //Document = (new DocumentFactory()).CreateDocument();
+            //Document.HighlightingStrategy = HighlightingStrategyFactory.CreateHighlightingStrategy();
+
+            //Document = TheOneAndOnly.Document;
+            
+            // remove other listeners
+            //RemoveAllChangedHandlers();
+            if (PreviousInstance != null)
+                PreviousInstance.Dispose();
+            PreviousInstance = this;
+
+         //TheOneAndOnly.ClearAll();
+            //Document.UndoStack.ClearAll();
+            //TheOneAndOnly.Refresh();
+
+    //        TheOneAndOnly.Modified += new Sansguerre.ModifiedHandler(TheOneAndOnly_Modified);
+            //Document.DocumentChanged += new DocumentEventHandler(Document_DocumentChanged);
+        }
+
+        void TheOneAndOnly_Modified(object o, Sansguerre.ModifiedArgs args)
+        {
+            if (Changed != null)
+                Changed(this, args);
+        }
+
+
+        public void BeginUpdate()
+        {
+            TheOneAndOnly.BeginUndoAction();// Document.UndoStack.StartUndoGroup();
+        }
+
+        public void EndUpdate()
+        {
+            TheOneAndOnly.EndUndoAction();//Document.UndoStack.EndUndoGroup();
+        }
+
+        public string Text
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder(TheOneAndOnly.TextLength);
+                TheOneAndOnly.GetText(TheOneAndOnly.TextLength, sb);
+                return sb.ToString();
+            }
+            set
+            {
+                TheOneAndOnly.SetText(value); //Document.TextContent = value;
+            }
+        }
+
+        public int LineCount
+        {
+            get { return TheOneAndOnly.LineCount; }
+        }
+
+        public event EventHandler Changed;
+
+        public void Replace(int Start, int Length, string NewText)
+        {
+            TheOneAndOnly.Anchor = Start;
+            TheOneAndOnly.CurrentPos = Start+Length;
+            TheOneAndOnly.ReplaceSel(NewText);
+        }
+
+        /* public static implicit operator DefaultDocument(TextEditorDocumentWrapper W)
+        {
+            return W.Document;
+        } */
+
+
+        public void Dispose()
+        {
+            TheOneAndOnly.Modified -= new Sansguerre.ModifiedHandler(TheOneAndOnly_Modified);
+        }
+    }
+}
