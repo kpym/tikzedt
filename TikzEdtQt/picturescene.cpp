85ef5ea19ad4d042416ddf212db45330d68a8fce
t.willwacher <t.willwacher@gmail.com>
2014-03-23 23:04:38 +0000

    qt tests

--- TikzEdtQt/picturescene.cpp
+++ TikzEdtQt/picturescene.cpp
@@ -1,14 +1,28 @@
 #include "picturescene.h"
+#include <QGraphicsRectItem>
 
 PictureScene::PictureScene(QObject *parent) :
     QGraphicsScene(parent)
 {
+    //setSceneRect(-100,-100,200,200);
+    setBackgroundBrush(Qt::yellow);
     pdfView = new QGraphicsPixmapItem();
     this->addItem(pdfView);
+    pdfView->show();
+    pdfView->setPos(0,0);
+
+
+    QGraphicsScene *s = this;
+
+    QGraphicsRectItem *r = new QGraphicsRectItem(QRect(-50,-50,100,100));
+    this->addRect(QRect(-50,-50,100,100));
 }
 
 
 void PictureScene::onPdfImageReady(const QPixmap &pic)
 {
     pdfView->setPixmap(pic);
+
+    QRectF r = itemsBoundingRect();
+
 }
